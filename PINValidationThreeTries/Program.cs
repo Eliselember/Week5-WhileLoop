7011d01e69b3862ee0ab20ce9d737717d175f91c
Opilane TTHK <opilane@tthk.ee>
2021-09-29 09:52:58 +0300

    Whileloop

---
+++ PINValidationThreeTries/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PINValidationThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //programm küsib kasutajal sisestada oma PIN-koodi
+            //programm võrdleb sisestatud PIN-koodi arvuga 1234
+            //kui sisestatud PIN-kood on 1234,
+            //programm kuvab konsoolis "Tere tulemast!
+            //kui sisestatud PIN on vale, programm kuvab konsoolis
+            //"Vale PIN. Proovi uuesti"
+            //kasutajal on kolm katset
+
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta oma PIN:");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+                if (userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }  
+                else
+                {
+                    Console.WriteLine("vale PIN! Proovi uuesti!");
+                }   
+
+                
+            }
+            Console.WriteLine("Kena päeva!");
+
+
+
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada oma PIN-koodi
+            //programm võrdleb sisestatud PIN-koodi arvuga 1234
+            //kui sisestatud PIN-kood on 1234,
+            //programm kuvab konsoolis "Tere tulemast!
+            //kui sisestatud PIN on vale, programm kuvab konsoolis
+            //"Vale PIN. Proovi uuesti"
+            //*katsetearv on piiramatu
+            bool loopActive = true;
+            //boolean --> true/false
+            
+            while (loopActive)
+            {
+                Console.WriteLine("Sisesta oma PIN:");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+
+                if (userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    loopActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("vale PIN! Proovi uuesti!");
+                }
+               
+            }
+            Console.WriteLine("Kena päeva!");
+
+        }   
+    }
+}
